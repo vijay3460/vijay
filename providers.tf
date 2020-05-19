9afc37db28e801d1474ffead241a39ee2cfcbd0f
vijay3460 <53028540+vijay3460@users.noreply.github.com>
2020-05-19 17:37:50 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAREYLOO3FZQCHARR4"
-  secret_key = "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"
+  access_key = "AKIAREYLOO3F7PTAMTS5"
+  secret_key = "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"
   region = "ap-south-1"
 }
 
