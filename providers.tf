697bf256e8d6ca397c0d7234356432628fc6da12
vijay3460 <53028540+vijay3460@users.noreply.github.com>
2020-05-20 10:01:29 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  access_key = "AKIAREYLOO3F7PTAMTS5"
-  secret_key = "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"
-  region = "ap-south-1"
+  profile    = "${var.profile}"
+  region     = "${var.region}"
 }
 
