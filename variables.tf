1328d98cc1af6c3e69edb298f800d7e6f5dba95d
vijay3460 <53028540+vijay3460@users.noreply.github.com>
2020-05-20 12:58:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,39 @@
+variable "region" {
+        default = "ap-south-1"
+}
+
+variable "profile" {
+    description = "AWS credentials profile you want to use"
+    default = "default"
+}
+
+variable "domain_name" {
+  description = "enter domain name"
+  type        = string
+}
+
+variable "elasticsearch_version" {
+  description = "enter elastic search version"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "enter data node count"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "enter data node size"
+  type        = string
+}
+
+variable "volume_size" {
+  description = "enter volume size"
+  type        = string
+}
+
+
+
 variable "subnetid_1" {
   description = "enter subnet1"
   type        = string
@@ -7,11 +43,6 @@ variable "subnetid_2" {
   description = "enter subnet2"
   type        = string
 }
-variable "region" {
-        default = "ap-south-1"
-}
 
-variable "profile" {
-    description = "AWS credentials profile you want to use"
-    default = "default"
-}
+
+
